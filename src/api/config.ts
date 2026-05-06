5d679216c0d6657778751a7869c813c998e30e32
Maxim Sharkevich <67413544+SharkyJunior@users.noreply.github.com>
2026-05-06 19:31:27 +0300

    Update config.ts

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,17 +1,7 @@
-let userUrl: string;
-let emailUrl: string;
-let folderUrl: string;
-
 // чтобы можно было на локальной машине тестить
-if (location.hostname === "localhost") {
-  userUrl = `${location.protocol}//localhost:8081/api/v1`;
-  emailUrl = `${location.protocol}//localhost:8082/api/v1`;
-  folderUrl = `${location.protocol}//localhost:8083/api/v1`;
-} else {
-  userUrl = `${location.protocol}//${location.hostname}/api/v1`;
-  emailUrl = `${location.protocol}//${location.hostname}/api/v1`;
-  folderUrl = `${location.protocol}//${location.hostname}/api/v1`;
-}
+const userUrl = `${location.protocol}//${location.hostname}/api/v1`;
+const emailUrl = `${location.protocol}//${location.hostname}/api/v1`;
+const folderUrl = `${location.protocol}//${location.hostname}/api/v1`;
 
 export const USER_URL: string = userUrl;
 export const EMAIL_URL: string = emailUrl;
