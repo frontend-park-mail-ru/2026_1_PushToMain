0b63d67876304f12c812cda05d386c46c097b291
Maxim Sharkevich <67413544+SharkyJunior@users.noreply.github.com>
2026-05-13 21:53:05 +0300

    update endpoints

--- src/api/ApiEmail.ts
+++ src/api/ApiEmail.ts
@@ -1,46 +1,49 @@
 import { EMAIL_URL } from "./config";
 import { AppStorage } from "../App";
 /**
- * Отправляет GET-запрос на эндпоинт /emails.
+ * Отправляет GET-запрос на эндпоинт /inbox.
  */
 export async function getEmailAll(offset: number) {
-    try {
-        const response = await fetch(`${EMAIL_URL}/emails?limit=50&offset=${offset}`, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            credentials: "include",
-        });
+  try {
+    const response = await fetch(
+      `${EMAIL_URL}/inbox?limit=50&offset=${offset}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      },
+    );
 
-        if (response.ok) {
-            const data = await response.json();
-            return data;
-        }
-    } catch {
-        return null;
+    if (response.ok) {
+      const data = await response.json();
+      return data;
     }
+  } catch {
+    return null;
+  }
 }
 
 export async function sendEmail(data = {}) {
-    try {
-        const response = await fetch(`${EMAIL_URL}/send`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "X-CSRF-Token": AppStorage.csrfToken,
-            },
-            credentials: "include",
-            body: JSON.stringify(data),
-        });
+  try {
+    const response = await fetch(`${EMAIL_URL}/send`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRF-Token": AppStorage.csrfToken,
+      },
+      credentials: "include",
+      body: JSON.stringify(data),
+    });
 
-        if (response) {
-            const res = await response.json();
-            return res;
-        }
-    } catch {
-        return false;
+    if (response) {
+      const res = await response.json();
+      return res;
     }
+  } catch {
+    return false;
+  }
 }
 
 /*
@@ -48,128 +51,131 @@ export async function sendEmail(data = {}) {
 	private.HandleFunc("/emails/unread", h.MarkEmailsAsUnRead).Methods(http.MethodPut, http.MethodOptions)
     */
 export async function readEmail(email_ids: number[]) {
-    try {
-        const response = await fetch(`${EMAIL_URL}/emails/read`, {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json",
-                "X-CSRF-Token": AppStorage.csrfToken,
-            },
-            credentials: "include",
-            body: JSON.stringify({ email_ids: email_ids }),
-        });
+  try {
+    const response = await fetch(`${EMAIL_URL}/emails/read`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRF-Token": AppStorage.csrfToken,
+      },
+      credentials: "include",
+      body: JSON.stringify({ email_ids: email_ids }),
+    });
 
-        if (response.ok) {
-            return true;
-        }
-
-        return false;
-    } catch {
-        return false;
+    if (response.ok) {
+      return true;
     }
+
+    return false;
+  } catch {
+    return false;
+  }
 }
 
 export async function unReadEmail(email_ids: number[]) {
-    try {
-        const response = await fetch(`${EMAIL_URL}/emails/unread`, {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json",
-                "X-CSRF-Token": AppStorage.csrfToken,
-            },
-            credentials: "include",
-            body: JSON.stringify({ email_ids: email_ids }),
-        });
+  try {
+    const response = await fetch(`${EMAIL_URL}/emails/unread`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRF-Token": AppStorage.csrfToken,
+      },
+      credentials: "include",
+      body: JSON.stringify({ email_ids: email_ids }),
+    });
 
-        if (response.ok) {
-            return true;
-        }
-
-        return false;
-    } catch {
-        return false;
+    if (response.ok) {
+      return true;
     }
+
+    return false;
+  } catch {
+    return false;
+  }
 }
 
 export async function getEmailByID(ID: number) {
-    try {
-        const response = await fetch(`${EMAIL_URL}/emails/${ID}`, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                "X-CSRF-Token": AppStorage.csrfToken,
-            },
-            credentials: "include",
-        });
+  try {
+    const response = await fetch(`${EMAIL_URL}/emails/${ID}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRF-Token": AppStorage.csrfToken,
+      },
+      credentials: "include",
+    });
 
-        if (response.ok) {
-            const data = await response.json();
-            return data;
-        }
-
-        return false;
-    } catch {
-        return false;
+    if (response.ok) {
+      const data = await response.json();
+      return data;
     }
+
+    return false;
+  } catch {
+    return false;
+  }
 }
 
 export async function getEmailSend(offset: number) {
-    try {
-        const response = await fetch(`${EMAIL_URL}/myemails?limit=50&offset=${offset}`, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            credentials: "include",
-        });
+  try {
+    const response = await fetch(
+      `${EMAIL_URL}/myemails?limit=50&offset=${offset}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      },
+    );
 
-        if (response.ok) {
-            const data = await response.json();
-            return data;
-        }
-    } catch {
-        return null;
+    if (response.ok) {
+      const data = await response.json();
+      return data;
     }
+  } catch {
+    return null;
+  }
 }
 
 export async function seacrhEmail(data: string) {
-    try {
-        const response = await fetch(`${EMAIL_URL}/emails/search`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "X-CSRF-Token": AppStorage.csrfToken,
-            },
-            credentials: "include",
-            body: JSON.stringify(data),
-        });
+  try {
+    const response = await fetch(`${EMAIL_URL}/emails/search`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRF-Token": AppStorage.csrfToken,
+      },
+      credentials: "include",
+      body: JSON.stringify(data),
+    });
 
-        if (response) {
-            const data = await response.json();
-            return data;
-        }
-    } catch {
-        return false;
+    if (response) {
+      const data = await response.json();
+      return data;
     }
+  } catch {
+    return false;
+  }
 }
 
 export async function uploadFile(file: File, emailId: number) {
-    try {
-        const formData = new FormData();
-        formData.append("file", file);
+  try {
+    const formData = new FormData();
+    formData.append("file", file);
 
-        const response = await fetch(`${EMAIL_URL}/emails/send/${emailId}/file`, {
-            method: "POST",
-            headers: {
-                "X-CSRF-Token": AppStorage.csrfToken,
-            },
-            credentials: "include",
-            body: formData,
-        });
-        if (response.ok) {
-            return true;
-        }
-    } catch {
-        return null;
+    const response = await fetch(`${EMAIL_URL}/emails/send/${emailId}/file`, {
+      method: "POST",
+      headers: {
+        "X-CSRF-Token": AppStorage.csrfToken,
+      },
+      credentials: "include",
+      body: formData,
+    });
+    if (response.ok) {
+      return true;
     }
+  } catch {
+    return null;
+  }
 }
