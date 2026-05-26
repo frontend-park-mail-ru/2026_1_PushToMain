c7347b8a21b661ce0027db337adf4d25cffaaa30
Maxim Sharkevich <67413544+SharkyJunior@users.noreply.github.com>
2026-05-26 23:12:49 +0300

    added separate webpack.prod.js

---
+++ webpack.common.js
@@ -0,0 +1,73 @@
+import path, { dirname } from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import CopyWebpackPlugin from "copy-webpack-plugin";
+
+const __dirname = dirname("./");
+
+export default {
+  entry: {
+    main: "./src/App.tsx",
+  },
+
+  output: {
+    path: path.resolve(__dirname, "./build"),
+    publicPath: "/",
+    clean: true,
+  },
+
+  resolve: {
+    extensions: [".tsx", ".jsx", ".ts", ".js"],
+    alias: {
+      "@react": path.resolve(__dirname, "src/react"),
+    },
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                "@babel/preset-env",
+                "@babel/preset-typescript",
+                [
+                  "@babel/preset-react",
+                  {
+                    pragma: "Death13.createElement",
+                    pragmaFrag: "Death13.Fragment",
+                  },
+                ],
+              ],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.svg$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+      filename: "index.html",
+      favicon: "./public/assets/svg/favicon.svg",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "public/sw.js", to: "sw.js" },
+        {
+          from: "**/*",
+          to: "assets/",
+          context: "public/assets/",
+          noErrorOnMissing: true,
+        },
+      ],
+    }),
+  ],
+};
